bec113075535f5f28aecdf162fe0ef04f7d5f31c
visibait <33099726+visibait@users.noreply.github.com>
2021-05-15 15:19:18 +0200

    VB-McDonald's: Uploaded Spanish Version

---
+++ Spanish Version/VB-McDonalds/config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.Zones = {
+    vector3(272.58, -964.31, 29.31),
+}
---
+++ Spanish Version/VB-McDonalds/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'VisiBait#0712'
+description 'McDonalds System by VB-Scripts'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'config.lua',
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+files {
+    'html/*.html',
+    'html/css/*.css',
+    'html/js/*.js',
+}
