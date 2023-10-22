26070def1dad14f2776a6e5a7c73c9417ef46697
XxLuciferModdingxX <147756651+XxLuciferModdingxX@users.noreply.github.com>
2023-10-22 23:39:54 +0200

    Update main.lua
    for esx eport on esx legacy

--- English Version/VB-McDonalds/server/main.lua
+++ English Version/VB-McDonalds/server/main.lua
@@ -1,5 +1,5 @@
 ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+ESX = exports["es_extended"]:getSharedObject()
 
 ItemsPlayerCanBuy = { -- Prevents Exploiting...
   "combomeal",
